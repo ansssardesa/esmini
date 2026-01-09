3fead6167a051d45214c940379d525e3ca556e84
Sai Sardesai <sai.sardesai@ansys.com>
2026-01-09 15:28:03 +0000

    Revert "debug domain check"
    This reverts commit 13184411605be95434427a4959f149ab48e3eaaf.

--- EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/Entities.cpp
+++ EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/Entities.cpp
@@ -283,15 +283,7 @@ int Object::OverrideController(const char* controller_name)
     {
         new_controller->LinkObject(this);
         AssignController(new_controller);
-        // verify controller is active for both longitudinal and lateral domains
-        auto longMask = static_cast<unsigned int>(ControlDomain2DomainMask(ControlDomains::DOMAIN_LONG));
-        auto latMask  = static_cast<unsigned int>(ControlDomain2DomainMask(ControlDomains::DOMAIN_LAT));
-
-        if (new_controller->IsActiveOnDomains(longMask) && new_controller->IsActiveOnDomains(latMask))
-        {
-            return 0;
-        }
-        return -1;
+        return 0;
     }
 
     return -1;
